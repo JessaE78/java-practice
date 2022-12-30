9c38e4aa66406e6918182da31c021c088563450e
JessaE78 <jecle440@gmail.com>
2022-12-29 23:47:24 -0500

    added java file

---
+++ stringPractice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class stringPractice {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input string: ");
+        String input = scan.nextLine();
+
+        System.out.println("--------- STATS ---------");
+        System.out.println("Length: " + input.length());
+
+    }
+
+    public void reverseString(){
+
+    }
+}
