4321854f28ab363abee5b8d7e28f522fd30af073
JessaE78 <jecle440@gmail.com>
2023-01-05 22:55:31 -0500

    Added java practice stuff

--- stringPractice.java
+++ stringPractice.java
@@ -1,4 +1,8 @@
+// Refreshing some java stuff while practicing getting comfy with git commands
+
+import java.util.Arrays;
 import java.util.Scanner;
+import java.io.*;
 
 public class stringPractice {
     public static void main(String[] args){
@@ -8,10 +12,40 @@ public class stringPractice {
 
         System.out.println("--------- STATS ---------");
         System.out.println("Length: " + input.length());
+        System.out.println("Reversed: " + reverseString(input));
+        System.out.println("Max recurring char: " + maxChar(input));
 
+
+        scan.close();
     }
 
-    public void reverseString(){
+    public static String reverseString(String userString){
+        String reversed = "";
 
+        for(int i = userString.length() - 1; i >=0; i--){
+            reversed += userString.charAt(i);
+        }
+        return reversed;
     }
+
+    public static char maxChar(String userString){
+        char maxRecur = ' ';
+
+        int counter[] = new int[256];
+        int max = 0;
+
+        for(int i = 0; i < userString.length(); i++){
+            counter[userString.charAt(i)]++;
+        }
+
+        for(int i = 0; i < userString.length(); i++){
+            if(max < counter[userString.charAt(i)]){
+                max = counter[userString.charAt(i)];
+                maxRecur = userString.charAt(i);
+            }
+        }
+
+        return maxRecur;
+    }
+
 }
